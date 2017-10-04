4c5d828a85682905e5bfd115eca05b49853e54a5
Ryan Sites <ryan.sites@optum.com>
2017-10-04 08:57:10 -0400

    global navigation

--- src/app/sdk/shared/models/globalNavigation.ts
+++ src/app/sdk/shared/models/globalNavigation.ts
@@ -1,3 +1,24 @@
-export const globalNavigation = {
-    links: []
+export const navigation = {
+    links: [
+        {
+            'label': 'Home',
+            'icon': 'fa fa-home',
+            'url': '/',
+            'children': [],
+            'tooltip': 'Home',
+            'show': false,
+            'protected': false,
+            'roles': []
+        },
+         {
+            'label': 'Study Guides',
+            'icon': 'fa fa-book',
+            'url': '/studyguide',
+            'children': [],
+            'tooltip': 'Study Guide',
+            'show': false,
+            'protected': false,
+            'roles': []
+        }
+    ]
 };
