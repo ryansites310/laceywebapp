e77de6fc2239ee83e320b90131192c580ca1f054
ryansites310 <ryan.r.sites@gmail.com>
2017-11-08 08:41:23 -0500

    Update environment.test.ts

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,3 @@
 export const environment = {
-  production: true,
-  apiBaseUrl : 'https://api-lacey-test.ose-ctc-core.optum.com/',
-  apiVersion : 'v1.0',
-  nodeApi : 'https://ccat-test.optum.com/profile'
+  production: true  
 };
