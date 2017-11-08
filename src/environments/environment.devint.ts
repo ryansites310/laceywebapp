f3a3c7f2833c403368e54195b6d8e62ecef40677
ryansites310 <ryan.r.sites@gmail.com>
2017-11-08 08:42:26 -0500

    Update environment.devint.ts

--- src/environments/environment.devint.ts
+++ src/environments/environment.devint.ts
@@ -1,6 +1,3 @@
 export const environment = {
-  production: true,
-  apiBaseUrl : 'https://api-lacey-dev.ose-ctc-core.optum.com/',
-  apiVersion : 'v1.0',
-  nodeApi : 'https://ccat-dev.optum.com/profile'
+  production: true
 };
